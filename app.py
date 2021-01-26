b5b7722b098f853f6bf64cbe5d53fbc7f482b671
leersmathieu <leers.mathieu@gmail.com>
2021-01-26 19:22:13 +0100

    new model with 128x128 resize

--- app.py
+++ app.py
@@ -5,20 +5,32 @@ import numpy as np
 import cv2
 from tensorflow.keras.models import load_model
 
+# important variables
+
+input_size_1 = 128
+input_size_2 = 128
+dim = 3
+
+######################
+
+row1_1, row1_2 = st.beta_columns((2, 4))
+
 
 def conversion(img):
     x = cv2.imdecode(np.frombuffer(img.read(), dtype=np.uint8), 1)
-    x = cv2.resize(x, (64, 64))
+    x = cv2.resize(x, (input_size_1, input_size_2))
+    st.write("The computer see...")
+    st.image(x, width=500)
     x = x / 255.
-    print(x.dtype)
     img_data = np.expand_dims(x, axis=0)
     return img_data
 
 
 def predict(img_data):
-    model = load_model('model/pneumonia_A87_R92_AUC94.h5')
+    model = load_model('model/pneumonia_A88_R94_AUC95_128x128.h5')
     classes = model.predict(img_data)
     print(classes)
+    st.write(classes[0][0])
     result = np.round(classes[0][0])
     print(result)
     return result
@@ -27,12 +39,11 @@ def predict(img_data):
 uploaded_file = st.file_uploader("Choose an image...", type="jpeg")
 if uploaded_file is not None:
     st.write('You selected `%s`' % uploaded_file)
-    st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+    st.image(uploaded_file, caption="Uploaded Image", width=500)
     st.write("")
     st.write("Analyzing...")
     res = conversion(uploaded_file)
     pred = predict(res)
-    st.write(pred)
     if pred < 0.5:
         st.write("Result is Normal")
 
